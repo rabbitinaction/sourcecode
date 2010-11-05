d2cee5d0fa4ceb7fceec7dd4906d88cd18a8a3ad
Alvaro Videla <videlalvaro@gmail.com>
2010-11-05 11:05:15 -0600

    removed old example files

--- 2.3.2.config.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-define('HOST', 'localhost');
-define('PORT', 5672);
-define('USER', 'guest');
-define('PASS', 'guest');
-define('VHOST', '/');
--- default_amqp_conf.php
+++
@@ -1,6 +0,0 @@
-<?php
-define('HOST', 'localhost');
-define('PORT', 5672);
-define('USER', 'guest');
-define('PASS', 'guest');
-?>
