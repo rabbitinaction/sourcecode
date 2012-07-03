2d237ba29cc4c7bc498217371a5b9e952e5e84c3
Christoph Jasinski <christoph.jasinski@googlemail.com>
2012-07-03 17:48:18 +0200

    modifying code so it's similar to the python example

--- ruby/chapter-2/hello_world_consumer.rb
+++ ruby/chapter-2/hello_world_consumer.rb
@@ -3,18 +3,22 @@ if defined?(Bundler); Bundler.setup; else; require "rubygems"; end
 require "amqp"
 
 EventMachine.run do
+  
   AMQP.connect(:host => "localhost", :username => "guest", :password => "guest") do |connection|
     puts "Connected. Waiting for messages... Hit Control + C to stop me."
 
     channel  = AMQP::Channel.new(connection)
     exchange = channel.direct("hello-exchange", :durable => true, :auto_delete => false)
     channel.queue("hello-queue", :durable => true, :auto_delete => false).bind(exchange, :routing_key => "hola").subscribe do |header, body|
-      puts "Received #{body}"
-
-      connection.close { EventMachine.stop }
+      
+      if body == "quit"
+        connection.close { EventMachine.stop }
+      else
+        puts body
+      end
     end
 
-    # stop on Control + c or after 1 second
+    # stop on Control + c
     stop = Proc.new { connection.close { EventMachine.stop } }
     Signal.trap("INT", &stop)
   end
